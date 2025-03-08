90e0d3c2b03ff4c6725dc556a502920840684a41
VamsiKrishna <krishvamsi321@gmail.com>
2025-03-08 18:59:28 +0530

    Class samples

--- Question-1.py
+++ Question-1.py
@@ -1,12 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow import keras
-from keras.datasets import fashion_mnist
-import wandb
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-from keras.datasets import mnist
+from utils import *
 
 # Load dataset
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
